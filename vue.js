6e4120473561220dc1878c401ccebcec965562ab
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-12-01 23:57:28 +0200

    update (1:13:) added Vue buttons. Works. W10D3

--- vue.js
+++ vue.js
@@ -1,1 +1,22 @@
 // (:56:)W10D3
+// (1:02:)W10D3
+new Vue({
+    el: '#vue-wrapper',
+    data: {
+        text: "This is text from Vue instance",
+        clickCount: 0
+    },
+    // (1:09:)
+    methods: {
+        addOne: function()  {
+            this.clickCount++;
+            console.log("clicked +");
+        },
+        removeOne: function()  {
+            this.clickCount--;
+            console.log("clicked -");
+            // this.clickCount -= 1;
+            // this.clickCount = this.clickCount -1;
+        }
+    }
+})
